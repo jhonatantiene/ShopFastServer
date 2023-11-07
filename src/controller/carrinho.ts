efef16f7a848d3d1fa49cff9f0445ef6874e072e
jhonatantiene <91148527+jhonatantiene@users.noreply.github.com>
2023-11-07 14:24:18 -0300

    .

--- src/controller/carrinho.ts
+++ src/controller/carrinho.ts
@@ -7,23 +7,21 @@ export class carrinho {
     }
 
     static async carrinhoCreate(param: any) {
-        if('idCliente == clienteAtual') {
-            if('idproduto == produtoAtual') {
-               return this.carrinhoUpdate(param)
+        if ('idCliente == clienteAtual') {
+            if ('idproduto == produtoAtual') {
+                return this.carrinhoUpdate(param)
             }
         }
         return await serviceDb.simplifiedQuery('INSERT INTO carrinho (idcarrinho, idProduto, idCliente, qtd, data) VALUES (?, ?, ?, ?, ?)', [2, 2, 2, 4, '20231025'])
     }
 
-
     static async carrinhoUpdate(param: any) {
         let carrinho: any = await this.carrinhoRead('param.user')
         let resultRead: any = 0
-    carrinho.forEach((v: any) => { resultRead = v.qtd + 1/*param.qtd*/ })
+        carrinho.forEach((v: any) => { resultRead = v.qtd + 1/*param.qtd*/ })
         console.log(resultRead)
         return await serviceDb.simplifiedQuery('UPDATE carrinho SET qtd = ?', [resultRead])
     }
-
 }
 
 export default carrinho;
